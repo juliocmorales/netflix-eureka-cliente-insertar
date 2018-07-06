01da24bac376cc2044b801cdbcbb88d31a0c34b0
juliocmorales <40004698+juliocmorales@users.noreply.github.com>
2018-07-06 13:22:23 -0500

    primer commit

---
+++ src/main/java/com/neoris/microservicios/NetflixEurekaClienteInsertarApplication.java
@@ -0,0 +1,14 @@
+package com.neoris.microservicios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class NetflixEurekaClienteInsertarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixEurekaClienteInsertarApplication.class, args);
+	}
+}
